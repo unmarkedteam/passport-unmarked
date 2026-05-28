0ad78d76058449c6ffdb81fec0ceb13a3499b330
unmarkedteam <team@unmarked.au>
2026-05-29 04:31:45 +1000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app'
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import Admin from './Admin'
+
+const isAdmin = window.location.pathname === '/admin'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  isAdmin ? <Admin /> : <App />
+)
